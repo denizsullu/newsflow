860728d75a6627df267e13891011796f13a5b60c
Deniz Sullu <deniz.sullu@alphastellar.io>
2025-07-23 12:52:24 +0300

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-
     plugins: [react()],
     server: {
         port: 3000,
-        host:true,
+        host: true,
         proxy: {
-            "/api": {
-                target: "http://deneme:8080"
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
             }
         }
     }
